68b39633ec571d6a30da876a9a6ce0af73c9fc76
LorinDeath <alexseylyou@gmail.com>
2026-01-07 22:50:24 +0200

    сурвеп

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   // ... ваши текущие настройки
+  output: 'server',
   adapter: cloudflare({
     // Добавьте эту опцию, чтобы исправить ошибку с sharp
     imageService: 'compile',
